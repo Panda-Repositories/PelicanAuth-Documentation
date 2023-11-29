3b777318edf9f52f63b65d8df91d771c190f1d54
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2023-11-29 16:28:54 +0800

    Update Example.py
    Nehh

--- API/Python/Example.py
+++ API/Python/Example.py
@@ -1,1 +1,2 @@
-#Due to concern to other developer, The API is onl available on your Dashboard
+# Due to concern to other developer, The API is only available on your Pelican's Dashboard or
+# Discord Server ( Pelican Development Community ) at Other Libraries. Be sure that you're Regular Membership or you can't access the lib
