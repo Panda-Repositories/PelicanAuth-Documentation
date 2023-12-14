6258550fc219d57945eff54260ecdd6c961fb862
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2023-12-14 19:37:38 +0800

    Added Exception Handling

--- Other/ADBInit.lua
+++ Other/ADBInit.lua
@@ -1,32 +1,41 @@
 game.StarterGui:SetCore("SendNotification", {
     Title = "Panda ADB-Sideload (Beta)"; -- the title (ofc)
     Text = "Successfully Executed"; -- what the text says (ofc)
-    Icon = "rbxassetid://11354741327"; -- the image if u want. 
+    Icon = "rbxassetid://5179834418"; -- the image if u want. 
     Duration = 3; -- how long the notification should in secounds
     })
 
 warn('Panda ADB-Development ( Windows ADB Sideloaded) for Delta')
 local key = "Ready"
 local scriptFilePath = "executed_script.lua"
-
-function ExecuteScriptCore(script)
-    local identifiershit = identifyexecutor();
-    if identifiershit == "Delta Android" then
-        runcode(script)
-    else
-        loadstring(script)() -- Universal bruhh
-    end
-end
+writefile(scriptFilePath, tostring(key))
 
 while true do
-    wait(0.5) -- Wait for 1 second before checking again
+    wait(0.2) -- Wait for 1 second before checking again
     local content = readfile(scriptFilePath)
+    
     if not content or content ~= key then
         if content then
-            -- loadstring(content)() -- Execute the script if content is not null and not equal to the key
-            ExecuteScriptCore(content)
+            -- Attempt to execute the script in a protected call
+            local success, result = pcall(function()
+                local identifiershit = identifyexecutor()
+                if identifiershit == "Delta Android" then
+                    runcode(script)
+                else
+                    loadstring(script)() -- Universal bruhh
+                end
+            end)
+
+            -- Check if the execution was successful
+            if not success then
+                -- Handle the error (replace this with your specific error-handling logic)
+                print("[Error Executing Script]: " .. result)
+            else
+                print("[Script Executed]")
+            end
         end
-        wait(1) -- Wait for 1 second before checking again
         writefile(scriptFilePath, tostring(key)) -- Update the file with the key
     end
 end
+
+
