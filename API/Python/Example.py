2dbb63ed9f69792ddac27dd4e3e3be9a8cc8963b
Sponsoparnordvpn <146754550+Sponsoparnordvpn@users.noreply.github.com>
2023-12-22 08:14:09 +0100

    Update Example.py

--- API/Python/Example.py
+++ API/Python/Example.py
@@ -1,43 +1,63 @@
-# Necessaries Library on Python to operate this code
-import importlib.util
-import urllib.request
-import sys
+# pip install panda-auth==0.3
 
-# --------------------------------------------------------------------------------------------
-# ( Important Module ) Include this to your project
-# --------------------------------------------------------------------------------------------
-def load_module_from_url(url, module_name):
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        request = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(request)
-        code = response.read().decode('utf-8')
-        module_spec = importlib.util.spec_from_loader(module_name, loader=None)
-        module = importlib.util.module_from_spec(module_spec)
-        exec(code, module.__dict__)
-        module.__file__ = url
-        sys.modules[module_name] = module
-        return module
+from panda_auth import *
 
-    except Exception as e:
-        print(f"Error loading module from URL: {e}")
-        return None
-      
-# --------------------------------------------------------------------------------------------
-# Load PandaAuthPYLib from the URL ( DO NOT RENAME THESE OR THE LIBRARY WON'T LOAD)
-# --------------------------------------------------------------------------------------------
-panda_auth_lib = load_module_from_url('https://auth.pandadevelopment.net/library/PythonLib/PandaAuthPYLib.py', 'PandaAuthPYLib')
+# Variables
 
-#Rename this Service name to your own
-service_name = "pandadevkit"
-
-# Get the Hardware ID from the current device (Don't use this if you're running on a server)
-panda_auth_lib.get_hardware_id() 
-
-# Returns True / False (boolean) Validate the Key of the User based on the hardware id and it's status.
-panda_auth_lib.validate_key(user_input, client_hardware_id, service_name)
-
-#Automatic added to your clipboard at the same time, returning as a string of URL (panda)
-panda_auth_lib.get_key(service_name, client_hardware_id)
+service = "pandadevkit"
 
 
+
+# reset hardware ID
+
+key = "blablabla"
+
+newhwid = "lmao"
+
+reset_hwid(service,key,newhwid, debug=True) # Enabling debug will help you to solve the errors
+
+
+
+# Create a premium key
+
+
+
+params = {
+
+'email': 'email.example@gmail.com', # Account email
+
+    'pass': '', # Account password 
+
+    'expire': '2050-12-24', # key expire
+
+    'note': 'Thanks for buying!', # key note 
+
+    'count': '1' # Number of key 
+
+}
+
+generate_key(params, debug=True)
+
+
+
+
+
+# Send the get key link without providing an hwid
+
+
+
+
+
+get_key(service, debug=True)
+
+
+
+# Get key with an HWID
+
+
+
+hwid="hwidhere"
+
+
+
+get_keyHWID(service, hwid, debug=True)
