71dcfa4fe01b4995c477579b6a8f2d68b09463e2
Sponsoparnordvpn <146754550+Sponsoparnordvpn@users.noreply.github.com>
2023-12-22 08:11:45 +0100

    Update Example.py

--- Examples/Python/Example.py
+++ Examples/Python/Example.py
@@ -1,54 +1,33 @@
-import importlib.util
-import urllib.request
-import sys
+# pip install panda-auth==0.3
+from panda_auth import *
+# Variables
+service = "pandadevkit"
 
-#Rename this Service name to your own
-service_name = "pandadevkit"
-# --------------------------------------------------------------------------------------------
-# ( Important Module ) Include this to your project
-# --------------------------------------------------------------------------------------------
-def load_module_from_url(url, module_name):
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        request = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(request)
-        code = response.read().decode('utf-8')
-        module_spec = importlib.util.spec_from_loader(module_name, loader=None)
-        module = importlib.util.module_from_spec(module_spec)
-        exec(code, module.__dict__)
-        module.__file__ = url
-        sys.modules[module_name] = module
-        return module
+# reset hardware ID
+key = "blablabla"
+newhwid = "lmao"
+reset_hwid(service,key,newhwid, debug=True) # Enabling debug will help you to solve the errors
 
-    except Exception as e:
-        print(f"Error loading module from URL: {e}")
-        return None
-# --------------------------------------------------------------------------------------------
-# Load PandaAuthPYLib from the URL ( DO NOT RENAME THESE OR THE LIBRARY WON'T LOAD)
-# --------------------------------------------------------------------------------------------
-url = 'https://auth.pandadevelopment.net/library/PythonLib/PandaAuthPYLib.py'
-module_name = 'PandaAuthPYLib'
-panda_auth_lib = load_module_from_url(url, module_name)
+# Create a premium key
 
-# Check if the library was successfully loaded
-if panda_auth_lib:
-    #The Fun Begin
-    def main():
-        while True:
-            client_hardware_id = panda_auth_lib.get_hardware_id()
-            user_input = input('Please enter your key. If you don\'t have a key, type "key": ')
-            if user_input.lower() == 'key':
-                panda_auth_lib.get_key(service_name, client_hardware_id)
-            else:
-                result = panda_auth_lib.validate_key(user_input, client_hardware_id, service_name)
-                if result:
-                    print('[ The Key is Valid ]')
-                    # Execute your favorite code (Python code here)
-                    break
-                else:
-                    print('[ The Key is Invalid ]')
+params = {
+'email': 'email.example@gmail.com', # Account email
+    'pass': '', # Account password 
+    'expire': '2050-12-24', # key expire
+    'note': 'Thanks for buying!', # key note 
+    'count': '1' # Number of key 
+}
+generate_key(params, debug=True)
 
-    if __name__ == "__main__":
-        main()
-else:
-    print("Unable to load the PandaAuthPYLib from the URL.")
+
+# Send the get key link without providing an hwid
+
+
+get_key(service, debug=True)
+
+# Get key with an HWID
+
+hwid="hwidhere"
+
+get_keyHWID(service, hwid, debug=True)
+
